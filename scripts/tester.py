cfe606542aad3f4cca0062fd87d58787be8392ef
Eric Nie <niee16@student.westboroughk12.org>
2016-11-13 00:22:30 -0500

    Replace Djikstra with Floyd-Warshall

--- scripts/tester.py
+++ scripts/tester.py
@@ -3,10 +3,9 @@ from compute_graph import *
 
 def main():
 	# clean and analyze the data
-	analyze()
-
-	# generate the graph for some amount requested by the user
-	compute(generate(10000))
+    countries = analyze()
+	#generate the graph for some amount requested by the user
+    compute(generate(10000,countries))
 
 if __name__ == "__main__":
-	main()
+    main()
