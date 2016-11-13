4fb8372713ea3be1fda48d2b32488d68609648ba
rpadaki <roshanpadaki@gmail.com>
2016-11-12 23:48:25 -0500

    Set up web server.

--- FlaskWebProject/graphs.py
+++ FlaskWebProject/graphs.py
@@ -18,17 +18,3 @@ def graph():
 
 if __name__ == "__main__":
 	app.run()
-
-import d3py
-import networkx as nx
-import logging
-import pathfindertesting
-logging.basicConfig(level=logging.DEBUG)
-paths, opt = pathfindertesting.test()
-g=nx.DiGraph()
-for start in opt:
-	for end in opt[start]:
-		g.add_edge(start,end,weight=opt[start][end])
-with d3py.NetworkXFigure(g,width=500, height=500) as p:
-	p += d3py.ForceLayout()
-	p.show()
