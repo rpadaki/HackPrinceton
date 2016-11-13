9f51efac1af896ee0a306046a7ccd0937a9abcce
Eric Nie <enie16@student.westboroughk12.org>
2016-11-12 21:59:38 -0500

    Completed script for calculating minimum links given cost

--- scripts/cleanData.py
+++ scripts/cleanData.py
@@ -9,30 +9,37 @@ CATEGORIES = ["source_code", "source_name", "source_income", "destination_code",
 CATEGORY_NUM = [2, 3, 5, 8, 9, 14, 15, 25, 32]
 class Country:
 
-
     def __init__(self, country_id):
         self.id = country_id
         self.income_group = ""
         self.links = {}
+        self.costs = {}
 
-    #Add links in the form (destination, fixed_cost, slope)
-    def addLink(self, country_to, fixed_cost, slope):
-        if country_to is not in self.links:
+    #Add links in the form (destination, total_cost_200, total_cost_500)
+    def addLink(self, country_to, total_cost_200, total_cost_500):
+        if not country_to in self.links:
             self.links[country_to] = []
-        self.links[country_to].append((fixed_cost, slope))
+        self.links[country_to].append((total_cost_200, total_cost_500))
 
     #Returns the links with the least cost based on the amount to send
+    def calculateCost(cost_200, cost_500, amount):
+        return (5/300 * cost_500 - 2/300 * cost_200) * amount + (-10/3 * cost_500 + 10/3 * cost_200)
+
     def getMinLink(self, send_amount, country_dest):
-        minTrip = (sys.maxsize, sys.maxsize, sys.maxsize)
-        for cost in country_dest:
-            fixed_cost = self.links[country_dest][cost][0]
-            slope = self.links[country_dest][cost][1]
-            percent_cost = (fixed_cost + slope * send_amount)/(send_amount)
+        for country in self.links:
+            min_cost = sys.maxsize
+            for pair in self.links[country]:
+                print(pair)
+                try:
+                    cost_200 = float(pair[0])
+                    cost_500 = float(pair[1])
+                except ValueError:
+                    continue
+                min_cost = min(min_cost, Country.calculateCost(cost_200, cost_500, send_amount))
+            self.costs[country] = min_cost
 
-
-
-    def toJSON(self):
-
+def find_index(category):
+    return CATEGORY_NUM[CATEGORIES.index(category)]
 
 if __name__=="__main__":
     countries = {}
@@ -49,6 +56,13 @@ if __name__=="__main__":
             header = lines[0].split(',')
             for row in range(1, len(lines)):
                 data_split = lines[row].split(',')
-                countries[data_split[CATEGORIES.index("source_code")]].addLink(data_split[CATEGORIES.index("destination_code")],
+                countries[data_split[find_index("source_code")]].addLink(data_split[find_index("destination_code")], data_split[find_index("total_cost_200")], data_split[find_index("total_cost_500")])
 
+    for country in countries:
+        for country_dest in countries[country].links:
+            countries[country].getMinLink(400., country_dest)
+
+    for country in countries:
+        print(country, end=" ")
+        print(countries[country].costs)
 
